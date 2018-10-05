687c238e64f46ad36223ae348767af65305eb53a
smithtrain <stan80smith@yahoo.com>
2018-10-05 17:50:23 -0500

    HomeworkSubmission

---
+++ 07-SocialAnalytics/config.py
@@ -0,0 +1,12 @@
+consumer_key ="WRDcVy65sHPOQLWuiZrXT5ajU"
+#API_Key ="WRDcVy65sHPOQLWuiZrXT5ajU"
+
+consumer_secret ="X5moMODX9kZtOLwlIpU1cKaCMaK1UW1v2tWsTKa85FdcTcl0YU"
+#API_Secret ="X5moMODX9kZtOLwlIpU1cKaCMaK1UW1v2tWsTKa85FdcTcl0YU"
+
+access_token ="1016816820450447361-TJkwat5aQQuWgGPaz6mBfrmjRdWrWr"
+
+access_token_secret ="7yLLGnlUsKLAOmc99X0I36Scr3pGHcn8q8sQBculMFiwh"
+
+Owner = "stan80smith"
+Owner_ID = "1016816820450447361"
